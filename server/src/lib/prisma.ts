851b0c18f5935246b1438523746c6a3a7bc44c3a
Levin010 <levinomwenga10@gmail.com>
2026-03-03 15:51:24 +0300

    Debugging comments

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -11,3 +11,13 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient }
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+// apparent fix if i use rds, to bypass adapter issues
+
+// const pool = new Pool({ 
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: true, // Now we ARE verifying
+//     ca: fs.readFileSync('/root/Rent1000-fullstack/server/global-bundle.pem').toString(),
+//   }
+// })
